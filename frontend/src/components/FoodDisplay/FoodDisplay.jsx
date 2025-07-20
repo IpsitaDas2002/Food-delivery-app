8a5d339588695e277baf80e8e943aad505458c44
Ipsita Das <ipsdas465@gmail.com>
2025-07-20 23:01:34 +0530

    food-del

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
-import { StoreContext } from '../../../context/StoreContext'
+import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
+import { food_list } from '../../assets/assets'
 
 const FoodDisplay = ({category}) => {
 
@@ -10,7 +11,7 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top dishes near you</h2>
-        <div className='food-display list'>
+        <div className='food-display-list'>
             {food_list.map((item,index)=>{
                 return <FoodItem key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
             })}
