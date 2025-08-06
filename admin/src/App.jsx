57f389e5b1e26014ed486f1b04315133bdacaa67
Ipsita Das <ipsdas465@gmail.com>
2025-08-06 21:28:15 +0530

    updated

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+import { Route, Routes } from 'react-router-dom'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
 
 const App = () => {
   return (
@@ -7,6 +13,11 @@ const App = () => {
       <hr/>
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' elememt={<Add/>}/>
+          <Route path='/list' elememt={<List/>}/>
+          <Route path='/orders' elememt={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
